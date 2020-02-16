8dab1202741d9d4fb5ae8578cfcda84f7ce1fba6
colormotor <drand48@gmail.com>
2020-02-16 01:52:50 +0000

    additional examples

--- example_square.py
+++ example_square.py
@@ -15,47 +15,17 @@ reload(axidraw_client)
 
 from axidraw_client import AxiDrawClient
 
-
-def lissajous(t, a, b, omega, delta):
-    return np.vstack([a*np.cos(omega*t + delta),
-                      b*np.sin(t)])
-
-def lissajous_glyph():
-    n = 200
-    S = []
-    
-    #t = np.linspace(0, np.pi*3.8, n)
-    t = np.linspace(0, np.pi*3.8, n)
-
-    delta = np.random.uniform(-np.pi/2, np.pi/2)
-    da = np.random.uniform(-np.pi/2, np.pi/2)
-    db = np.random.uniform(-np.pi/2, np.pi/2)
-    omega = 2.
-    for o in np.linspace(0, 0.2, 2):
-        a = np.sin(np.linspace(0, np.pi*2, n) + da + o*0.5)*100
-        b = np.cos(np.linspace(0, np.pi*2, n) + db + o*1.0)*100  
-        P = lissajous(t, a,
-                         b,
-                         omega,
-                         delta)
-        S.append(P)
-    return S, delta, da, db
-
-
-# Generate
-S, delta, da, db = lissajous_glyph()
-
+square = np.array([[0.0, 0.0], [1.0, 0.0], [1.0,1.0], [0.,1.0]]).T
+S = [square]
+close = True
 #######################################
 # Send to axidraw
 # Comment this block out to just plot the output
-server_addr = '192.168.153.23'
 server_addr = 'localhost'
 
 client = AxiDrawClient(address=server_addr, port=80)
 # Simple send a list of paths and close connection
-client.draw_paths(S, title='TEST') #'d=%.2f da=%.2f db=%.2f'%(delta, da, db))
-
-
+client.draw_paths(S, title='TEST', close=close) #'d=%.2f da=%.2f db=%.2f'%(delta, da, db))
 
 
 # Alternative, send paths and block until all paths have been drawn
